aaa0bdb4faea27944c167a057a711f95d5f1a908
Michael Pumo <hello@michaelpumo.com>
2020-03-01 22:31:51 +0000

    Activate event to clear SW caches.

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const version = '1.0.1'
+const version = '1.0.2'
 const cacheName = `app-${version}`
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
         '/'
@@ -12,16 +12,14 @@ self.addEventListener('install', e => {
   )
 })
 
-// self.addEventListener('activate', event => {
-//   event.waitUntil(self.clients.claim())
-// })
-
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.open(cacheName)
-//       .then(cache => cache.match(event.request, { ignoreSearch: true }))
-//       .then(response => {
-//         return response || fetch(event.request)
-//       })
-//   )
-// })
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName) {
+          return caches.delete(key)
+        }
+      }))
+    })
+  )
+})
