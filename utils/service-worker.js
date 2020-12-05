e56e23cccfa6217e49d0e962749f33184948086c
Michael Pumo <hello@michaelpumo.com>
2020-12-05 16:31:10 +0000

    Remove service worker console logs.

--- utils/service-worker.js
+++ utils/service-worker.js
@@ -1,14 +1,11 @@
 function serviceWorker() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('/service-worker.js', { scope: '/' })
-      .then((registration) => {
-        console.log('Service Worker Registered')
-      })
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+    // .then((registration) => {
+    // })
 
-    navigator.serviceWorker.ready.then((registration) => {
-      console.log('Service Worker Ready')
-    })
+    // navigator.serviceWorker.ready.then((registration) => {
+    // })
   }
 }
 
