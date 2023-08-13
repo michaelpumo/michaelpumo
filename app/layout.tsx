de58a97bf08960a5f836ef2c87c1c2e98c0d0e89
Michael Pumo <hello@michaelpumo.com>
2023-08-13 22:28:45 +0100

    Fixes. WTF

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,11 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Michael Pumo',
-  description: 'Portfolio',
+  description: 'Portfolio'
 }
 
 export default function RootLayout(
-  { children }: { children: React.ReactNode },
+  { children }: { children: React.ReactNode }
 ) {
   return (
     <html lang="en-GB">
