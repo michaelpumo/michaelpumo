2aeb00e86f2fc28c1a6296077d2616f1eaec34fb
Michael Pumo <hello@michaelpumo.com>
2023-08-14 18:52:16 +0100

    Compress.

--- components/Statement.tsx
+++ components/Statement.tsx
@@ -14,13 +14,11 @@ const Statement: FC<Props> = ({ blok }) => {
     <section
       {...storyblokEditable(blok)}
       data-component="Statement"
-      className=" w-full min-h-screen bg-white"
+      className="w-full min-h-screen bg-white flex items-center justify-center px-4 sm:px-10 lg:px-14 xl:px-20"
     >
-      <div className="w-full min-h-screen flex items-center justify-center">
-        <h2 className="font-display text-7xl text-balance text-center text-brand-blue mb-10">
-          Wonderful things happen when you work together.
-        </h2>
-      </div>
+      <h2 className="font-display text-3xl sm:text-7xl text-balance text-center text-brand-blue mb-10">
+        Wonderful things happen when you work together.
+      </h2>
     </section>
   )
 }
