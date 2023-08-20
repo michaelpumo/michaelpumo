3d526eb822f9cdb5fe3866d0171edbf956e6a308
Michael Pumo <hello@michaelpumo.com>
2023-08-20 23:23:46 +0100

    Adjust for mobile.

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -42,10 +42,10 @@ const Clients: FC<Props> = ({ blok }) => {
     <section
       {...storyblokEditable(blok)}
       data-component="Clients"
-      className="relative z-10 grid grid-cols-2 gap-4 xs:gap-8 sm:gap-16 xl:gap-20 w-full min-h-screen bg-brand-grey-light text-brand-dark p-4 xs:p-8 sm:p-16 xl:p-20"
+      className="relative z-10 grid md:grid-cols-2 gap-10 sm:gap-16 xl:gap-20 w-full min-h-screen bg-brand-grey-light text-brand-dark p-4 xs:p-8 sm:p-16 xl:p-20"
     >
       <div>
-        <div className="sticky top-4 xs:top-8 sm:top-16 xl:top-20 z-10">
+        <div className="md:sticky md:top-16 xl:top-20 md:z-10">
           <h1 className="text-3xl mb-5">Clients</h1>
 
           <p className="text-2xl text-brand-dark mb-5 text-balance max-w-[40ch]">
@@ -53,31 +53,37 @@ const Clients: FC<Props> = ({ blok }) => {
             course of my web development career.
           </p>
 
-          <p className="text-lg text-brand-dark/50 mb-10 max-w-[40ch]">
+          <p className="text-lg text-brand-dark/50 max-w-[40ch]">
             From content-managed static sites with a headless CMS to eCommerce
             shopping experiences.
           </p>
         </div>
       </div>
 
-      <ul className="flex flex-col gap-1 text-6xl">
-        {blok.clients &&
-          blok.clients.map((client: any, index: number) => (
-            <li
-              {...storyblokEditable(client)}
-              key={index}
-              ref={el => (itemsRef.current[index] = el)}
-              data-index={index}
-              tabIndex={0}
-              onMouseEnter={() => setIndex(index)}
-              className={`transition-all duration-500 ease-outExpo ${
-                index === currentIndex ? 'translate-x-8 lg:translate-x-10' : '' // opacity-10'
-              }`}
-            >
-              {client.title}
-            </li>
-          ))}
-      </ul>
+      <div>
+        {/* <img src="https://source.unsplash.com/200x200/?designstudio" alt="" /> */}
+
+        <ul className="flex flex-col gap-1 text-5xl lg:text-6xl">
+          {blok.clients &&
+            blok.clients.map((client: any, index: number) => (
+              <li
+                {...storyblokEditable(client)}
+                key={index}
+                ref={el => (itemsRef.current[index] = el)}
+                data-index={index}
+                tabIndex={0}
+                onMouseEnter={() => setIndex(index)}
+                className={`transition-all duration-500 ease-outExpo ${
+                  index === currentIndex
+                    ? 'translate-x-8 lg:translate-x-10'
+                    : '' // opacity-10'
+                }`}
+              >
+                {client.title}
+              </li>
+            ))}
+        </ul>
+      </div>
     </section>
   )
 }
