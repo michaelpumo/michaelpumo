737ca0a69eea9fa886367b8347f1ca7be855c3e7
Michael Pumo <hello@michaelpumo.com>
2023-10-10 16:55:58 +0100

    Get latest Storyblok information.

--- types/storyblok.d.ts
+++ types/storyblok.d.ts
@@ -67,9 +67,65 @@ export interface PageStoryblok {
   [k: string]: any;
 }
 
+export type MultilinkStoryblok =
+  | {
+      cached_url?: string;
+      linktype?: string;
+      [k: string]: any;
+    }
+  | {
+      id?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "story";
+      story?: {
+        name: string;
+        created_at?: string;
+        published_at?: string;
+        id: number;
+        uuid: string;
+        content?: {
+          [k: string]: any;
+        };
+        slug: string;
+        full_slug: string;
+        sort_by_date?: null | string;
+        position?: number;
+        tag_list?: string[];
+        is_startpage?: boolean;
+        parent_id?: null | number;
+        meta_data?: null | {
+          [k: string]: any;
+        };
+        group_id?: string;
+        first_published_at?: string;
+        release_id?: null | number;
+        lang?: string;
+        path?: null | string;
+        alternates?: any[];
+        default_full_slug?: null | string;
+        translated_slugs?: null | any[];
+        [k: string]: any;
+      };
+      [k: string]: any;
+    }
+  | {
+      url?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "asset" | "url";
+      [k: string]: any;
+    }
+  | {
+      email?: string;
+      linktype?: "email";
+      [k: string]: any;
+    };
+
 export interface ProjectStoryblok {
   title?: string;
   image?: AssetStoryblok;
+  link?: MultilinkStoryblok;
   _uid: string;
   component: "project";
   [k: string]: any;
